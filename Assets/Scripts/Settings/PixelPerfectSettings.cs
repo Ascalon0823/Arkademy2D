f80d935126b5696cc8d03d18738c137cc476adb3
Ascalon <ascalon0823@gmail.com>
2024-06-22 09:45:46 +0800

    add pixel perfect camera and canvas

---
+++ Assets/Scripts/Settings/PixelPerfectSettings.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PixelPerfectSettings", menuName = "PixelPerfect/CreateNewSettings", order = 1)]
+public class PixelPerfectSettings : ScriptableObject
+{
+    public int pixelPerUnit;
+    public Vector2 referenceResolution;
+    public bool useReferredScale;
+    public int pixelScale;
+    public static PixelPerfectSettings GetSettings()
+    {
+        return Resources.LoadAll<PixelPerfectSettings>("Settings").FirstOrDefault();
+    }
+}
