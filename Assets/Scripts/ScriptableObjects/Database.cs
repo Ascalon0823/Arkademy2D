9a1e0b62ebbd0f9a7c321e1e5fabc684c8ffe0b4
Ascalon <ascalon0823@gmail.com>
2024-09-14 13:02:40 +0800

    update version, add enemy and wave data

--- Assets/Scripts/ScriptableObjects/Database.cs
+++ Assets/Scripts/ScriptableObjects/Database.cs
@@ -42,9 +42,37 @@ namespace Arkademy
             public Sprite uiIcon;
         }
 
+        [Serializable]
+        public class EnemyData
+        {
+            public string name;
+            public EnemyBehaviour prefab;
+            public Sprite uiIcon;
+            public int health;
+            public int power;
+            public int speed;
+            public int xp;
+        }
+
+        [Serializable]
+        public class StageData
+        {
+            public string stageName;
+            public WaveData[] waveData;
+        }
+
+        [Serializable]
+        public class WaveData
+        {
+            public int minimumEnemy;
+            public int[] spawnableEnemy;
+        }
+
         public List<PlayableCharacterData> playableCharacterData = new List<PlayableCharacterData>();
         public List<AbilityData> abilityData = new List<AbilityData>();
         public List<SpellData> spellData = new List<SpellData>();
+        public List<EnemyData> enemyData = new List<EnemyData>();
+        public List<StageData> stageData = new List<StageData>();
 
         public static Database GetDatabase()
         {
