7ea7c8fd6f170cd874f9935056e13fcd96ad830a
Ascalon <ascalon0823@gmail.com>
2024-09-22 10:08:15 +0800

    tile map procedurally generate around camera view port

--- Assets/Scripts/WangTilemap.cs
+++ Assets/Scripts/WangTilemap.cs
@@ -13,33 +13,58 @@ namespace Arkademy
         [SerializeField] private Tile[] palette;
 
         [SerializeField] private Tilemap renderMap;
-
+        public Vector2 offset;
+        public Transform cameraPos;
+        public int gridSize = 10;
+        public Vector2Int gridPos;
 
         private void Start()
         {
-            var data = new int[101, 101];
-            var offset = Random.insideUnitCircle * Random.Range(1000, 5000);
-            
-            for (var y = 0; y < 101; y++)
-            for (var x = 0; x < 101; x++)
-            {
-                data[x, y] = Mathf.PerlinNoise(x * 8.0f / 101 + offset.x, y * 8.0f / 101 + offset.y) > 0.5 ? 1 : 0;
-            }
-
-            SetData(data);
+            offset = Random.insideUnitCircle * Random.Range(1000, 5000);
+            UpdateMap(true);
         }
 
-        public void SetData(int[,] data)
+        private void Update()
+        {
+            UpdateMap();
+        }
+
+        public void UpdateMap(bool force = false)
+        {
+            var newGridPos = new Vector2Int(Mathf.RoundToInt(cameraPos.position.x / gridSize),
+                Mathf.RoundToInt(cameraPos.position.y / gridSize));
+            if (newGridPos == gridPos && !force) return;
+            gridPos = newGridPos;
+            var size = gridSize * 3;
+            var data = new int[size + 1, size + 1];
+            var halfSize = size / 2;
+            var origin = new Vector2Int(newGridPos.x * gridSize - halfSize, newGridPos.y * gridSize - halfSize);
+            for (var y = 0; y <= size; y++)
+            for (var x = 0; x <= size; x++)
+            {
+                data[x, y] =
+                    Mathf.PerlinNoise((origin.x + x) * 8.0f / (size + 1) + offset.x,
+                        (origin.y + y) * 8.0f / (size + 1) + offset.y) > 0.5
+                        ? 1
+                        : 0;
+            }
+
+            SetData(data, origin);
+        }
+
+        public void SetData(int[,] data, Vector2Int origin)
         {
             for (var y = 0; y < data.GetLength(1) - 1; y++)
             for (var x = 0; x < data.GetLength(0) - 1; x++)
             {
                 var key = 0;
                 key += data[x, y] << 0;
-                key += data[x+1, y] << 1;
-                key += data[x, y+1] << 2;
+                key += data[x + 1, y] << 1;
+                key += data[x, y + 1] << 2;
                 key += data[x + 1, y + 1] << 3;
-                renderMap.SetTile(new Vector3Int(x, y, 0), palette[key]);
+                renderMap.SetTile(
+                    new Vector3Int(origin.x + x, origin.y + y, 0),
+                    palette[key]);
             }
         }
     }
