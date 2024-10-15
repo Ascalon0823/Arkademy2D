7c2d7e7b51ffb083a90696eede3df5762d195540
Ascalon <ascalon0823@gmail.com>
2024-10-15 10:23:01 +0800

    Add equipment template and prefabs

---
+++ Assets/Arkademy/Templates/ScriptableObjects/EquipmentTemplate.cs
@@ -0,0 +1,26 @@
+using Arkademy.Data;
+using UnityEngine;
+
+namespace Arkademy.Templates
+{
+    [CreateAssetMenu(fileName = "New Equipment Template", menuName = "Template/Equipment", order = 0)]
+    public class EquipmentTemplate : ScriptableObject
+    {
+        public Equipment templateData;
+        public Behaviour.Equipment equippedPrefab;
+        [Header("Graphics")] public Sprite equipmentSprite;
+        public RuntimeAnimatorController equipmentAnimation;
+        public Behaviour.EquipmentGraphic equippedGraphicPrefab;
+        public Sprite equipmentUISprite;
+        
+        
+        private void OnEnable()
+        {
+            templateData.templateName = name;
+        }
+        public Equipment GetNewEquipment()
+        {
+            return new Equipment(templateData);
+        }
+    }
+}
