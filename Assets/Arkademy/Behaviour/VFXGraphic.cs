c5e7b390108ec6bf3c92f6464ba54b680457d4ff
Ascalon <ascalon0823@gmail.com>
2024-10-30 15:44:08 +0800

    add graphic flip for vfx

--- Assets/Arkademy/Behaviour/VFXGraphic.cs
+++ Assets/Arkademy/Behaviour/VFXGraphic.cs
@@ -3,10 +3,33 @@ using UnityEngine;
 
 namespace Arkademy.Behaviour
 {
+    [ExecuteInEditMode]
     public class VFXGraphic : MonoBehaviour
     {
+        public SpriteRenderer spriteRenderer;
         public Animator animator;
         public float playSpeed;
+        public bool flipY;
+        public bool flipX;
+        public Vector2 flipYRefAxis;
+        public Vector2 flipYUseAxis;
+        public Vector2 flipXRefAxis;
+        public Vector2 flipXUseAxis;
+
+        private void Update()
+        {
+            if (flipY)
+            {
+                spriteRenderer.flipY =
+                    Vector2.Dot(transform.TransformDirection(flipYUseAxis), flipYRefAxis.normalized) < 0;
+            }
+
+            if (flipX)
+            {
+                spriteRenderer.flipX =
+                    Vector2.Dot(transform.TransformDirection(flipXUseAxis), flipXRefAxis.normalized) < 0;
+            }
+        }
 
         private void OnEnable()
         {
