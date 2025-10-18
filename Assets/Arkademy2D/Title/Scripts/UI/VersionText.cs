2cd5e7beaf7ef98df4613208476b5923e7de15ff
arkademy <ascalon0823@gmail.com>
2025-10-18 15:15:31 +0800

    Add basic title scene

---
+++ Assets/Arkademy2D/Title/Scripts/UI/VersionText.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Arkademy2D.Title.UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class VersionText : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<TextMeshProUGUI>().text = Application.version;
+        }
+    }
+}
