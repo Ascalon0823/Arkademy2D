7a50d4cdc07718dc416357736500b0de3cbe5d2a
arkademy <ascalon0823@gmail.com>
2025-11-09 18:22:56 +0800

    Add relay support

---
+++ Assets/Arkademy2D/Tests/Multiplayer/Relay/Scripts/TestRelayUI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using Utp;
+
+namespace Arkademy2D.Tests.Relay
+{
+    public class TestRelayUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField connectionCodeText;
+
+        public void Host()
+        {
+            (RelayNetworkManager.singleton as RelayNetworkManager).StartRelayHost(5);
+        }
+
+        protected void Update()
+        {
+            var joinCode =  (RelayNetworkManager.singleton as RelayNetworkManager).relayJoinCode;
+            if (!string.IsNullOrEmpty(joinCode))
+            {
+                connectionCodeText.text = joinCode;
+            }
+        }
+
+        public void Join()
+        {
+            var manager =
+                RelayNetworkManager.singleton as RelayNetworkManager;
+            manager.relayJoinCode = connectionCodeText.text;
+            manager.JoinRelayServer();
+        }
+    }
+}
