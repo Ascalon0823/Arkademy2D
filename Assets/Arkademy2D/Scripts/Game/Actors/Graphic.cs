4b68adb427066edcc69dc51d53902048026ffb76
arkademy <ascalon0823@gmail.com>
2025-11-30 13:35:09 +0800

    add graphic control on actor

---
+++ Assets/Arkademy2D/Scripts/Game/Actors/Graphic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Arkademy2D.Game.Actors
+{
+    public class Graphic : MonoBehaviour
+    {
+        public SpriteRenderer sprite;
+        public Animator animator;
+
+        public Movement movement;
+
+        public bool spriteFaceLeft;
+
+        private void LateUpdate()
+        {
+            animator.SetBool("walking", movement.moveDir.sqrMagnitude > float.Epsilon);
+            sprite.flipX = Vector2.Dot(movement.moveDir, Vector2.right) > 0 ? spriteFaceLeft : !spriteFaceLeft;
+        }
+    }
+}
