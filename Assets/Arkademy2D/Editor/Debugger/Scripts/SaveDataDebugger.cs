339ac265ff5d2dbe3778e6a62bc3139f592f94d8
Dust <ascalon0823@Gmail.com>
2025-12-15 15:32:27 +0800

    move debugger into editor folder

---
+++ Assets/Arkademy2D/Editor/Debugger/Scripts/SaveDataDebugger.cs
@@ -0,0 +1,88 @@
+using System.Linq;
+using Arkademy2D.Game.Data.Runtime;
+using Arkademy2D.Core.Models;
+using Arkademy2D.Game.Data.Static.Item;
+using Arkademy2D.Game.System;
+using Newtonsoft.Json;
+using UnityEditor;
+using UnityEngine;
+
+namespace Arkademy2D.Editor.Debugger
+{
+    [CreateAssetMenu(fileName = "SaveDataDebugger", menuName = "Debugger/SaveDebugger")]
+    public class SaveDataDebugger : ScriptableObject
+    {
+        [TextArea(3, 100000)] public string playerDataJson;
+
+        public string PlayerDataJson
+        {
+            get => playerDataJson;
+            set
+            {
+                playerDataJson = value;
+                ForceUpdateInspector();
+            }
+        }
+        public string playerDataKey;
+
+        private void ForceUpdateInspector()
+        {
+            EditorUtility.SetDirty(this);
+        }
+        [ContextMenu("New")]
+        public void NewPlayer()
+        {
+            var data = new PlayerData
+            {
+                PlayerModel = new Player()
+            };
+            PlayerDataJson = JsonConvert.SerializeObject(data, Formatting.Indented);
+        }
+        
+        [ContextMenu("Load all")]
+        public void LoadAllPlayers()
+        {
+            var players = SaveSystem.LoadAllPlayers();
+            foreach (var player in players)
+            {
+                Debug.Log(player.PlayerModel.Key);
+            }
+        }
+
+        [ContextMenu("Load")]
+        public void LoadPlayer()
+        {
+            PlayerDataJson = JsonConvert.SerializeObject(SaveSystem.LoadPlayer(playerDataKey), Formatting.Indented);
+            ForceUpdateInspector();
+        }
+
+        [ContextMenu("Save")]
+        public void SavePlayer()
+        {
+            SaveSystem.SavePlayer(JsonConvert.DeserializeObject<PlayerData>(PlayerDataJson));
+        }
+
+        public string selectCharacterKey;
+        public ItemBase itemBaseToAdd;
+
+        [ContextMenu("Add item to character")]
+        public void AddItemToCharacter()
+        {
+            var playerData = JsonConvert.DeserializeObject<PlayerData>(PlayerDataJson);
+            var chara = playerData.PlayerModel.Characters.FirstOrDefault(x => x.Id.ToString() == selectCharacterKey);
+            if (chara == null)
+            {
+                Debug.Log($"Character {selectCharacterKey} not found");
+                return;
+            }
+
+            if (!itemBaseToAdd)
+            {
+                Debug.Log("No item base");
+                return;
+            }
+            chara.Items.Add(new Item{ItemBaseId = itemBaseToAdd.Id});
+            PlayerDataJson =  JsonConvert.SerializeObject(playerData, Formatting.Indented);
+        }
+    }
+}
